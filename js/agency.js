0b56c3723896f8c2df45a606a905f3b555cc78b4
Leszek <lunardog@icloud.com>
2023-10-21 21:48:50 +0900

    agreements in a separate page

--- js/agency.js
+++ js/agency.js
@@ -9,7 +9,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $('#' + $anchor.attr('href').split('#')[1]).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
